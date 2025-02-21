5bb8677784978a049341332e5bb5ac83ad4da85b
Grace Stewart <gracestewart3@cmail.carleton.ca>
2025-02-21 15:03:55 -0500

    init

---
+++ addeventdialog.cpp
@@ -0,0 +1,14 @@
+#include "addeventdialog.h"
+#include "ui_addeventdialog.h"
+
+AddEventDialog::AddEventDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::AddEventDialog)
+{
+    ui->setupUi(this);
+}
+
+AddEventDialog::~AddEventDialog()
+{
+    delete ui;
+}
---
+++ addeventdialog.h
@@ -0,0 +1,22 @@
+#ifndef ADDEVENTDIALOG_H
+#define ADDEVENTDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class AddEventDialog;
+}
+
+class AddEventDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit AddEventDialog(QWidget *parent = nullptr);
+    ~AddEventDialog();
+
+private:
+    Ui::AddEventDialog *ui;
+};
+
+#endif // ADDEVENTDIALOG_H
